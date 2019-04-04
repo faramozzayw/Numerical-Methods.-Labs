62fd8c51d7dba01a9c60803ae7b2d54bd590026b
faramo_zayw <faramo.zayw@gmail.com>
2019-04-04 17:59:56 +0300

    Had translated into english

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -16,7 +16,7 @@ export default class  extends Component {
 		e.preventDefault();
 		console.clear();
 		if (this.state.fx.trim() === '' || this.state.a.trim() === '' || this.state.b.trim() === '' || this.state.h.trim() === '') {
-			alert('Помилка вводу: порожня строка'); 
+			alert('Input error. Check the correctness of the data.'); 
 			return;
 		}
 		const [fx, a, b, h] = [String(this.state.fx), Number(this.state.a), Number(this.state.b), Number(this.state.h)];
@@ -24,7 +24,7 @@ export default class  extends Component {
 
 		Number.isFinite(a) && Number.isFinite(b) && Number.isFinite(h) && h > 0  ? 
 			result = MyFunc(fx, a, b, h) :
-			alert('Перевірте правильність даних.');
+			alert('Input error. Check the correctness of the data.');
 
 		console.log(result);
 	}
@@ -34,37 +34,37 @@ export default class  extends Component {
 			<div className="uk-container">
 				<form>
 			    <fieldset className="uk-fieldset">
-			        <legend className="uk-legend">Введення даних</legend>
+			        <legend className="uk-legend">A inputting data</legend>
 			        <div className="uk-margin">
-			            <input 	className="uk-input uk-form-large" type="text" placeholder="Введіть рівняння" id="func"
+			            <input 	className="uk-input uk-form-large" type="text" placeholder="Input a function" id="func"
 													value={this.state.fx}
 													name='fx'
 													onChange={this.handleInputChange.bind(this)}
 									 />
 							</div> 
 							<div className="uk-margin">
-			            <input className="uk-input uk-form-large" type="text" placeholder="Введіть початок проміжку" id="first"
+			            <input className="uk-input uk-form-large" type="text" placeholder="Input a start of interval" id="first"
 			            				value={this.state.a}
 			            				name='a'
 													onChange={this.handleInputChange.bind(this)}
 			            				/>
 			         	</div>				
 			        <div className="uk-margin">
-			            <input className="uk-input uk-form-large" type="text" placeholder="Введіть кінець проміжку" id="end"
+			            <input className="uk-input uk-form-large" type="text" placeholder="Input a end of interval" id="end"
 			            				value={this.state.b}
 			            				name='b'
 													onChange={this.handleInputChange.bind(this)}
 			            				/>
 
 			        <div className="uk-margin">
-			            <input className="uk-input uk-form-large" type="text" placeholder="Введіть точність" id="h"
+			            <input className="uk-input uk-form-large" type="text" placeholder="Input a accuracy" id="h"
 			            				value={this.state.h}
 			            				name='h'
 													onChange={this.handleInputChange.bind(this)}
 			            				/>
 			         	</div>				
 			        		<button className="uk-button uk-button-primary"
-			        		onClick={this.handleClick.bind(this)}>Почати відокремлення кореня</button>
+			        		onClick={this.handleClick.bind(this)}>Start a root separation</button>
 			        </div>
 			    </fieldset>
 				</form>
